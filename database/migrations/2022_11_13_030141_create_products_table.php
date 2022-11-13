3582934e313ca7b40331a5d27ff560e957344113
jasonk19 <13520080@std.stei.itb.ac.id>
2022-11-13 11:43:14 +0700

    [FEAT] Create database migration

--- database/migrations/2022_11_13_030141_create_products_table.php
+++ database/migrations/2022_11_13_030141_create_products_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('product_name');
+            $table->string('product_slug');
+            $table->bigInteger('price');
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
